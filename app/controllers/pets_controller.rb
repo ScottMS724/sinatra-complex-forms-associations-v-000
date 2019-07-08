ef45af00ae842933a6477751e89d1ce81bf2371f
Scott Silverman <scottms724@gmail.com>
2019-07-08 16:51:35 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -16,7 +16,8 @@ class PetsController < ApplicationController
         @pet.owner = Owner.create(name: params["owner"]["name"])
       end
     @pet.save
-  end
+    redirect to "pets/#{@pet.id}"
+  end 
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
